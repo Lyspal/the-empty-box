96d2fc00ed2a70b6b6ada28b6932f6bd7c27b729
Sylvain Laporte <laporte.sylvain@siggraph.org>
2023-02-02 21:02:28 -0500

    Remove unused passthrough rule

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
 
-  eleventyConfig.addPassthroughCopy("src/images/");
   eleventyConfig.addPassthroughCopy("src/assets/");
 
   return {
