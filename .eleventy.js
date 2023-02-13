c578e558f599027c0714cee34e8b75e03a0c1461
Sylvain Laporte <laporte.sylvain@siggraph.org>
2023-02-13 03:30:11 -0500

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css/");
 
   eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
 
   // Filters
   eleventyConfig.addFilter("centsToDollars", (cents) => {
